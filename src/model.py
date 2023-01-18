3ddcac1d148345fa083b38ce135988e8222ed155
andishehtavakoli <andishehtavakoli@gmail.com>
2023-01-17 18:24:36 -0800

    update sentiment finance news_2

--- src/model.py
+++ src/model.py
@@ -8,25 +8,28 @@ from pathlib import Path
 from sentiment_analyzer import sentiment_analysis
 from data_preprocessing import text_preprocessing
 from loguru import logger
+import pickle
+from joblib import Parallel, delayed
+import joblib
 
 p= Path('./data')
 all_data_files = list(p.glob('**/*.csv'))
 
 
 
-def read_concat_create_df():
-    """read and concate two dataframes"""
+def read_df():
+    """read and concate and process two dataframes"""
 
     list_df = [pd.read_csv(path) for path in all_data_files]
 
     df = pd.concat(list_df , axis=0, ignore_index=True)
+    df = df.dropna()
+    df['Description'] = df['Description'].apply(text_preprocessing)
+    df = sentiment_analysis(df, 'Description')
 
     return df
 
-df = read_concat_create_df()
-df = df.dropna()
-df['Description'] = df['Description'].apply(text_preprocessing)
-df = sentiment_analysis(df, 'Description')
+
 
 
 def build_model(df):
@@ -38,18 +41,28 @@ def build_model(df):
     pipeline.fit(X_train, y_train)
     y_pred = pipeline.predict(X_test)
 
-    logger.info(f'train model score: {pipeline.score(X_train, y_train)}')
-    logger.info(f'test model score: {pipeline.score(X_test, y_test)}')
-    logger.info(f'accuracy score: {accuracy_score(y_test, y_pred)}')
+    # logger.info(f'train model score: {pipeline.score(X_train, y_train)}')
+    # logger.info(f'test model score: {pipeline.score(X_test, y_test)}')
+    # logger.info(f'accuracy score: {accuracy_score(y_test, y_pred)}')
 
     return pipeline
 
 
 if __name__ == '__main__':
 
+    df = read_df()
     pipeline_model = build_model(df)
     sent1 = ['GST officers detect Rs 4,000 crore of ITC fraud in April-June']
     sent2 = ["Finance Ministry releases Rs 9,871 crore to 17 states as grant"]
     print(pipeline_model.predict(sent1))
     print(pipeline_model.predict(sent2))
+
+
+
+
+    # Save the model as a pickle in a file
+    joblib.dump(pipeline_model, 'finance_news_model.pkl')
+
+
+
 
